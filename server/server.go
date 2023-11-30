0ee5e30e0f4c9d41880084b0e5684148b41bb3c0
bruce-mig <h180009t@hit.ac.zw>
2023-11-30 16:05:09 +0200

    added gRPC server reflection

--- server/server.go
+++ server/server.go
@@ -15,6 +15,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
 	"google.golang.org/grpc/status"
 )
 
@@ -63,6 +64,9 @@ func main() {
 	opts := []grpc.ServerOption{}
 	s := grpc.NewServer(opts...)
 	pb.RegisterBlogServiceServer(s, grpcBloggerServer)
+
+	//  Register reflection service on gRPC server.
+	reflection.Register(s)
 
 	go func() {
 		fmt.Println("Starting Server...")
